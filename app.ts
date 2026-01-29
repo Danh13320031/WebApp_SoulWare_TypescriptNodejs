6a0baaf10181554511c582b5537a9815ae9cb145
Danh13320031 <danh13320031@gmail.com>
2026-01-29 11:33:31 +0700

    #create: Successfully created a base route.

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("OK");
+import express, { Express } from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
