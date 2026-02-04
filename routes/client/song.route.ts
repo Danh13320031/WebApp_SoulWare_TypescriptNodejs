e2e9ca46fb9e2e1a0d4c37dcfbfe363d7e54f125
Danh13320031 <danh13320031@gmail.com>
2026-02-04 22:02:56 +0700

    #create: Successfully created a route for song detail page.

--- routes/client/song.route.ts
+++ routes/client/song.route.ts
@@ -3,5 +3,6 @@ import songController from "../../controllers/client/song.controller";
 const songRoute: Router = Router();
 
 songRoute.get("/:topicSlug", songController.getAllSongGet);
+songRoute.get("/detail/:songSlug", songController.getOneSongGet);
 
 export default songRoute;
