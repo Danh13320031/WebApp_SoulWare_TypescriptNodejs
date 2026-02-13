5d3827491399427bd6ad6a601e35e3fb8f89fae5
Danh13320031 <danh13320031@gmail.com>
2026-02-13 08:40:19 +0700

    #get: Successfully got a list of favorite songs, render ui and configured the system time.

---
+++ configs/systemTime.config.ts
@@ -0,0 +1,14 @@
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+import { Express } from "express";
+import { APP_TIMEZONE } from "../constants/constant";
+
+const timeConfig = (app: Express): void => {
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+  dayjs.tz.setDefault(APP_TIMEZONE);
+  app.locals.dayjs = dayjs;
+};
+
+export default timeConfig;
--- controllers/client/favoriteSong.controller.ts
+++ controllers/client/favoriteSong.controller.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from "express";
+import FavoriteSongModel from "../../models/favoriteSong.model";
 
-// [GET]: /favoritesongs/
+// [GET]: /favorite-songs/
 const getAllFavoriteSongGet = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
+  const favoriteSongList = await FavoriteSongModel.find({
+    // userId: req.user?._id, // Khi có xác thực người dùng thì tìm bài hát thích của tôi
+    status: "active",
+    deleted: false,
+  }).populate({
+    path: "songId",
+    select: "title avatar singerId slug like",
+    populate: { path: "singerId", select: "stageName slug" },
+  });
+
   res.render("client/pages/favoriteSong/favoriteSong.view.ejs", {
     pageTitle: "Playlist yêu thích của tôi",
+    favoriteSongList,
   });
 };
 
