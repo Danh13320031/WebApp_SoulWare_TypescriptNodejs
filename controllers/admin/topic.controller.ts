b09d61283d5f2288876952c98832898aff80d95b
Danh13320031 <danh13320031@gmail.com>
2026-02-17 20:36:25 +0700

    #admin-get: Successfully got the entire list of topics and rendered ui.

--- controllers/admin/topic.controller.ts
+++ controllers/admin/topic.controller.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express";
+import TopicModel from "../../models/topic.model";
 
 const getAllTopicGet = async (req: Request, res: Response): Promise<void> => {
+  const topicList = await TopicModel.find({
+    deleted: false,
+  }).select("-deleted -description");
+
   res.render("admin/pages/topic/topic.view.ejs", {
     pageTitle: "Danh sách chủ đề",
+    topicList,
   });
 };
 
