d5e464bdd91c7e7b844aa517175d3d309b8b4e02
Danh13320031 <danh13320031@gmail.com>
2026-02-17 21:51:40 +0700

    #admin-get: Successfully got the entire list of songs and rendered ui web admin.

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from "express";
+import SongModel from "../../models/song.model";
 
 // [GET]: /admin/songs
 const getAllSongGet = async (req: Request, res: Response): Promise<void> => {
+  const songList = await SongModel.find({
+    deleted: false,
+  })
+    .select("-deleted -description -audio -lyrics -slug")
+    .sort({ position: "desc" })
+    .populate("singerId", "stageName")
+    .populate("topicId", "title");
+
   res.render("admin/pages/song/song.view.ejs", {
     pageTitle: "Danh sách bài hát",
+    songList,
   });
 };
 
