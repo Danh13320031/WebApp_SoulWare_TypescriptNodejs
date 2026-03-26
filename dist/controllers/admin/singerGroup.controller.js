7d1febe304df5a524b502fa268bfb6b619f63148
Danh13320031 <danh13320031@gmail.com>
2026-03-26 22:03:49 +0700

    #admin-create: Successfully created base module for singer-group-service.

---
+++ dist/controllers/admin/singerGroup.controller.js
@@ -0,0 +1,38 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_status_codes_1 = require("http-status-codes");
+const activeSider_helper_1 = __importDefault(require("../../helpers/admin/activeSider.helper"));
+const singerGroupGet = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const pathname = (0, activeSider_helper_1.default)(req.originalUrl);
+        res.render("admin/pages/singerGroup/singerGroup.view.ejs", {
+            pageTitle: "Danh sách nhóm ca sĩ",
+            pathname,
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(http_status_codes_1.StatusCodes.INTERNAL_SERVER_ERROR).json({
+            code: http_status_codes_1.StatusCodes.INTERNAL_SERVER_ERROR,
+            status: "Fail",
+            message: "Server error - singerGroupGet",
+        });
+        return;
+    }
+});
+const singerGroupController = {
+    singerGroupGet,
+};
+exports.default = singerGroupController;
