47b8105abe9355b42f26ad60d80acd4c088b7491
Danh13320031 <danh13320031@gmail.com>
2026-03-28 21:47:01 +0700

    #admin-patch: Successfully updated a singer group by id.

--- controllers/admin/singerGroup.controller.ts
+++ controllers/admin/singerGroup.controller.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import { APP_ADMIN_PAGINATION_LIMIT } from "../../constants/app.constant";
 import activeSider from "../../helpers/admin/activeSider.helper";
+import handleSortFilter from "../../helpers/admin/handleSortFilter.helper";
 import handleStatusFilter from "../../helpers/admin/handleStatusFilter.helper";
 import convertTextToSlug from "../../helpers/convertTextToSlug.helper";
+import handlePagination from "../../helpers/handlePagination.helper";
 import SingerModel from "../../models/singer.model";
 import SingerGroupModel from "../../models/singerGroup.model";
-import { TStatusFilter } from "../../types/index.type";
-import { TDataBodyCreateSingerGroup } from "../../types/singerGroup.type";
-import handleSortFilter from "../../helpers/admin/handleSortFilter.helper";
+import { TPagination, TStatusFilter } from "../../types/index.type";
+import {
+  TDataBodyCreateSingerGroup,
+  TDataBodyUpdateSingerGroup,
+} from "../../types/singerGroup.type";
 
 // [GET]: /admin/singer-groups
 const singerGroupGet = async (req: Request, res: Response): Promise<void> => {
@@ -53,13 +58,35 @@ const singerGroupGet = async (req: Request, res: Response): Promise<void> => {
 
     const sortFilter = handleSortFilter(sort);
 
+    // Handle pagination
+    let page: number = 1;
+    let limit: number = APP_ADMIN_PAGINATION_LIMIT;
+    let type: string = "";
+
+    if (req.query.page) page = Number(req.query.page);
+    if (req.query.limit) limit = Number(req.query.limit);
+    if (req.query.type) type = req.query.type as string;
+
+    const count = await SingerGroupModel.countDocuments(find);
+
+    const pagination: TPagination = await handlePagination(
+      page,
+      limit,
+      type,
+      count,
+    );
+
     const singerGroupList = await SingerGroupModel.find(find)
+      .select("-deleted -createdAt -deletedAt -updatedAt -slug -__v")
       .sort(sortFilter.sortOptions)
-      .populate("singers", "fullName stageName");
+      .populate("singers", "fullName stageName")
+      .skip(pagination.skipPage)
+      .limit(pagination.limitPage);
 
     res.render("admin/pages/singerGroup/singerGroup.view.ejs", {
       pageTitle: "Danh sách nhóm ca sĩ",
       pathname,
+      pagination,
       singerGroupList,
       keyword,
       status,
@@ -146,16 +173,142 @@ const createANewSingerGroupPost = async (
   }
 };
 
+// [GET]: /admin/singer-groups/update/:singerGroupId
+const getASingerGroupByIdGet = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const pathname = activeSider(req.originalUrl);
+    let find: any = { deleted: false };
+    let singerGroupId: string = "";
+
+    if (req.params.singerGroupId)
+      singerGroupId = req.params.singerGroupId as string;
+
+    const singerGroup = await SingerGroupModel.findOne({
+      _id: singerGroupId,
+      ...find,
+    })
+      .select("-deleted -deletedAt -createdAt -updatedAt -slug -__v")
+      .populate("singers", "stageName");
+
+    if (!singerGroup) {
+      res.status(StatusCodes.NOT_FOUND).json({
+        code: StatusCodes.NOT_FOUND,
+        status: "Fail",
+        message: "Không tìm thấy nhóm ca sĩ",
+      });
+      return;
+    }
+
+    const singerList = await SingerModel.find(find).select("stageName");
+
+    const newSingerFromGroup = singerGroup.singers.map((singer) =>
+      singer._id.toString(),
+    );
+    const newSingerList = singerList.map((singer) => {
+      if (newSingerFromGroup.includes(singer._id.toString())) {
+        return {
+          ...singer,
+          checked: true,
+        };
+      } else {
+        return {
+          ...singer,
+          checked: false,
+        };
+      }
+    });
+
+    res.render("admin/pages/singerGroup/update.view.ejs", {
+      pageTitle: "Cập nhật nhóm ca sĩ",
+      singerGroup,
+      pathname,
+      newSingerList,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      code: StatusCodes.INTERNAL_SERVER_ERROR,
+      status: "Fail",
+      message: "Server error - getASingerGroupByIdGet",
+    });
+    return;
+  }
+};
+
+// [PATCH]: /admin/singer-groups/update/:singerGroupId
+const updateASingerGroupByIdPatch = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    let singerGroupId: string = "";
+    let avatar: string = "";
+
+    if (req.params.singerGroupId)
+      singerGroupId = req.params.singerGroupId as string;
+    if (req.body.avatar) avatar = req.body.avatar;
+
+    const singerGroup = await SingerGroupModel.findOne({
+      _id: singerGroupId,
+      deleted: false,
+    });
+
+    if (!singerGroup) {
+      res.status(StatusCodes.NOT_FOUND).json({
+        code: StatusCodes.NOT_FOUND,
+        status: "Fail",
+        message: "Không tìm thấy nhóm ca sĩ",
+      });
+      return;
+    }
+
+    const dataBodyUpdateSingerGroup: TDataBodyUpdateSingerGroup = {
+      avatar: avatar ? avatar : singerGroup.avatar,
+      name: req.body.name ? req.body.name : null,
+      description: req.body.description ? req.body.description : null,
+      status: req.body.status ? req.body.status : null,
+      singers: req.body.singers ? req.body.singers : null,
+    };
+
+    await SingerGroupModel.updateOne(
+      { _id: singerGroupId },
+      { $set: dataBodyUpdateSingerGroup },
+    );
+
+    res.status(StatusCodes.OK).json({
+      code: StatusCodes.OK,
+      status: "Success",
+      message: "Cập nhật nhóm ca sĩ thành cong!",
+    });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      code: StatusCodes.INTERNAL_SERVER_ERROR,
+      status: "Fail",
+      message: "Server error - updateASingerGroupByIdPatch",
+    });
+    return;
+  }
+};
+
 type TSingerGroupController = {
   singerGroupGet: (req: Request, res: Response) => Promise<void>;
   createANewSingerGroupGet: (req: Request, res: Response) => Promise<void>;
   createANewSingerGroupPost: (req: Request, res: Response) => Promise<void>;
+  getASingerGroupByIdGet: (req: Request, res: Response) => Promise<void>;
+  updateASingerGroupByIdPatch: (req: Request, res: Response) => Promise<void>;
 };
 
 const singerGroupController: TSingerGroupController = {
   singerGroupGet,
   createANewSingerGroupGet,
   createANewSingerGroupPost,
+  getASingerGroupByIdGet,
+  updateASingerGroupByIdPatch,
 };
 
 export default singerGroupController;
